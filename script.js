c1998189ea75af105da952500cffd49ac94bc24e
Joseph Nahas <joe_nahas@hotmail.com>
2024-12-08 23:42:50 -0500

    Added Product Property Methods

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ class ProductProperties{
         this.price = price; // price of product
         this.quantity = quantity; // quantity of product
     }
+
+    // Methods
+    getTotalValue(){ 
+        return this.price * this.quantity;
+    }
+
+    toString(){ // return a string including the product details
+        return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}.`;
+    }
 }
