3e55ff15b7f8246d09fc00be43b1b44a04f47c5f
Joseph Nahas <joe_nahas@hotmail.com>
2024-12-08 23:36:48 -0500

    Setup Product Properties Class

---
+++ script.js
@@ -0,0 +1,8 @@
+class ProductProperties{
+    // Constructor
+    constructor(name, price, quantity){
+        this.name = name; // name of product
+        this.price = price; // price of product
+        this.quantity = quantity; // quantity of product
+    }
+}
